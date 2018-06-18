f476fdad09cbbb2edb1955ec18cb39b9b5ca2cac
Wil Cornish <wilcoclahas@gmail.com>
2018-06-18 11:27:00 +0100

    will pass game_id test

---
+++ models/loan.rb
@@ -0,0 +1,13 @@
+class Loan
+
+  attr_reader( :id, :game_id, :customer_id, :returned, :day_borrowed)
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @customer_id = options['customer_id']
+    @game_id = options['game_id']
+    @returned = false
+    @day_borrowed = options['day_borrowed']
+  end
+
+end
---
+++ models/specs/loan_spec.rb
@@ -0,0 +1,21 @@
+require('minitest/autorun')
+require('pry-byebug')
+
+require_relative('../loan.rb')
+
+class LoanTest < MiniTest::Test
+
+  def setup
+    @loan1 = Loan.new({
+      'customer_id' => 1,
+      'game_id' => 1,
+      'day_borrowed' => "Satuday"
+      })
+  end
+
+  def test_has_game_id
+    assert_equal(1, @loan1.game_id)
+  end
+
+
+end
