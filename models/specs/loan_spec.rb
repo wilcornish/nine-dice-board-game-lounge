d136231763651a8defbc59aa6945bfb9f485e40e
Wil Cornish <wilcoclahas@gmail.com>
2018-06-18 11:31:05 +0100

    will pass day_borrowed test

--- models/specs/loan_spec.rb
+++ models/specs/loan_spec.rb
@@ -9,20 +9,24 @@ class LoanTest < MiniTest::Test
     @loan1 = Loan.new({
       'customer_id' => 1,
       'game_id' => 1,
-      'day_borrowed' => "Satuday"
+      'day_borrowed' => "Saturday"
       })
   end
 
   def test_has_game_id
-    assert_equal(1, @loan1.game_id)
+    assert_equal(1, @loan1.game_id())
   end
 
   def test_has_customer_id
-    assert_equal(1, @loan1.customer_id)
+    assert_equal(1, @loan1.customer_id())
   end
 
   def test_is_returned__false
-    assert_equal(false, @loan1.returned)
+    assert_equal(false, @loan1.returned())
+  end
+
+  def test_has_day_borrowed
+    assert_equal('Saturday', @loan1.day_borrowed())
   end
 
 end
