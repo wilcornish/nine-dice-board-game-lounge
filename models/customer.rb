ed113a0f1cadc9ea08fc69c875726ccb15a27cda
Wil Cornish <wilcoclahas@gmail.com>
2018-06-18 11:23:56 +0100

    adds customer class id

--- models/customer.rb
+++ models/customer.rb
@@ -3,6 +3,7 @@ class Customer
   attr_reader( :id, :badge_number, :name)
 
   def initialize( options )
+    @id = options['id'].to_i if options['id']
     @badge_number = options['badge_number'].to_i
     @name = options['name']
   end
