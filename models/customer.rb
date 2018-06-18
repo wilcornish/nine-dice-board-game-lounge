7f7d24892c394b85ab7aff59e7e3dc15c5e3d0cd
Wil Cornish <wilcoclahas@gmail.com>
2018-06-18 13:22:08 +0100

    will add database methods to customer class

--- models/customer.rb
+++ models/customer.rb
@@ -8,4 +8,46 @@ class Customer
     @name = options['name']
   end
 
+  def save()
+  sql = "INSERT INTO customers
+  (
+    name,
+    type
+  )
+  VALUES
+  (
+    $1, $2
+  )
+  RETURNING id"
+  values = [@name, @type]
+  results = SqlRunner.run(sql, values)
+  @id = results.first()['id'].to_i
+end
+
+def games
+  sql = "SELECT games.* FROM games INNER JOIN loans ON loans.game_id = games.id WHERE loans.customer_id = $1;"
+  values = [@id]
+  results = SqlRunner.run(sql, values)
+  return results.map { |game| Game.new(game) }
+end
+
+def self.all()
+  sql = "SELECT * FROM customers"
+  results = SqlRunner.run( sql )
+  return results.map { |hash| Customer.new( hash ) }
+end
+
+def self.find( id )
+  sql = "SELECT * FROM customers
+  WHERE id = $1"
+  values = [id]
+  results = SqlRunner.run( sql, values )
+  return Customer.new( results.first )
+end
+
+def self.delete_all
+  sql = "DELETE FROM customers"
+  SqlRunner.run( sql )
+end
+
 end
