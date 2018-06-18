9ca3c758a8fb7274a4f202e677c09727b2b7c804
Wil Cornish <wilcoclahas@gmail.com>
2018-06-18 11:47:52 +0100

    will add start of return method, requires db creation

--- models/specs/loan_spec.rb
+++ models/specs/loan_spec.rb
@@ -2,6 +2,7 @@ require('minitest/autorun')
 require('pry-byebug')
 
 require_relative('../loan.rb')
+require_relative('../game.rb')
 
 class LoanTest < MiniTest::Test
 
@@ -11,6 +12,14 @@ class LoanTest < MiniTest::Test
       'game_id' => 1,
       'day_borrowed' => "Saturday"
       })
+
+      @game1 = Game.new({
+        'name' => 'Mysterium',
+        'owner' => 'Wil',
+        'genre' => 'co-operative',
+        'player_count' => 5,
+        'play_time' => 45
+        })
   end
 
   def test_has_game_id
@@ -33,5 +42,11 @@ class LoanTest < MiniTest::Test
     @loan1.return
     assert_equal(true, @loan1.returned)
   end
+
+  # def test_check_in
+  #   @loan1.check_in
+  #   assert_equal(true, @loan1.returned)
+  #   assert_equal(1, @game1.times_played)
+  # end
 
 end
