bc786e8ea0ed348d20e409b50fc107e6aad1b568
Wil Cornish <wilcoclahas@gmail.com>
2018-06-19 11:01:14 +0100

    will add routes to game controller

--- controllers/game_controller.rb
+++ controllers/game_controller.rb
@@ -2,3 +2,34 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/game.rb' )
 also_reload( '../models/*' )
+
+get '/games' do
+  @games = Game.all()
+  erb ( :game/index )
+end
+
+get '/games/:id' do
+  @games = Game.find(params['id'].to_i)
+  erb ( :game/show )
+end
+
+get '/games/new' do
+  erb( :games/new )
+end
+
+get 'games/:id/edit' do
+  @game = Game.find(params[:id])
+  erb (:edit)
+end
+
+post '/games' do
+  game = Game.new(params)
+  game.save
+  redirect to ('/games')
+end
+
+put '/games/:id' do
+  game = Game.find(params['id'].to_i)
+  game.update(params)
+  redirect to ('/games/:id')
+end
