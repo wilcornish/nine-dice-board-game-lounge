c51f393b69f150c89192841d44b7b75198ef91dd
Wil Cornish <wilcoclahas@gmail.com>
2018-06-19 12:22:31 +0100

    will add routes to customer controller

--- controllers/customer_controller.rb
+++ controllers/customer_controller.rb
@@ -2,3 +2,40 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/customer.rb' )
 also_reload( '../models/*' )
+
+#show
+get '/customers' do
+  @customers = Customer.all()
+  erb ( :customer/index )
+end
+
+#index
+get '/customers/:id' do
+  @customers = Customer.find(params['id'].to_i)
+  erb ( :customer/show )
+end
+
+#new
+get '/customers/new' do
+  erb( :customers/new )
+end
+
+#edit
+get 'customers/:id/edit' do
+  @customer = Customer.find(params[:id])
+  erb (:edit)
+end
+
+#create
+post '/customers' do
+  customer = Customer.new(params)
+  customer.save
+  redirect to ('/customers')
+end
+
+#update
+put '/customers/:id' do
+  customer = Customer.find(params['id'].to_i)
+  customer.update(params)
+  redirect to ('/customers/:id')
+end
