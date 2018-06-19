eaf4a66e45be2206ea7d12a3f15b566bd721d10b
Wil Cornish <wilcoclahas@gmail.com>
2018-06-19 15:40:31 +0100

    will fix bugs in loan controller

--- controllers/loan_controller.rb
+++ controllers/loan_controller.rb
@@ -5,28 +5,29 @@ require_relative( '../models/game.rb' )
 require_relative( '../models/loan.rb' )
 also_reload( '../models/*' )
 
-#show
+#index
 get '/loans' do
   @loans = Loan.all()
   erb ( :'loans/index' )
 end
 
-#index
-get '/loans/:id' do
-  @loans = Loan.find(params['id'].to_i)
-  erb ( :'loans/show' )
-end
-
 #new
-get '/games/new' do
+get '/loans/new' do
   @games = Game.all()
   @customers = Customer.all()
   erb( :'loans/new' )
 end
 
+#show
+get '/loans/:id' do
+  @loan = Loan.find(params['id'].to_i)
+  erb ( :'loans/show' )
+end
+
 #create
-post '/games' do
-  loan = Loan.new(params)
-  loan.save
+post '/loans' do
+  game = Game.find("#{params['game_id']}")
+  customer = Customer.find("#{params['customer_id']}")
+  Loan.check_out(customer, game)
   redirect to ('/loans')
 end
