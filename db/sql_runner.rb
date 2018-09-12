04ed4c5715aa8b4833b4394f956cbd6203ba3b64
Wil Cornish <wilcoclahas@gmail.com>
2018-09-12 13:38:45 +0100

    corrected db host

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'nine_dice', host: 'localhost' })
+      db = PG.connect({ dbname: 'nine_dice', host: 'postgresql-corrugated-47018' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
