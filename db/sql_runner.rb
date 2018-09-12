16f1be3cf071beb95922bb9fb9693d4115ce33c8
Wil Cornish <wilcoclahas@gmail.com>
2018-09-12 14:18:11 +0100

    minor fix

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,8 +4,9 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'dfjf1fo20ep249', host: '
-        ec2-54-217-245-9.eu-west-1.compute.amazonaws.com', port: 5432,
+      db = PG.connect({ dbname: 'dfjf1fo20ep249',
+        host: 'ec2-54-217-245-9.eu-west-1.compute.amazonaws.com',
+        port: 5432,
         user: 'pszluyhnuippfs',
         password: '3927ddbdb4da372fef510e3529cbb27ca23099ed6bf329054f9195b9c6ac1bb5'})
         db.prepare("query", sql)
