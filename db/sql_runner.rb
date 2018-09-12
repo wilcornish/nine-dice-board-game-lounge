63f4ac88942417e6123dc6f023059f5a1d68aea9
Wil Cornish <wilcoclahas@gmail.com>
2018-09-12 13:49:01 +0100

    db host?

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'nine_dice', host: 'postgres://pszluyhnuippfs:3927ddbdb4da372fef510e3529cbb27ca23099ed6bf329054f9195b9c6ac1bb5@ec2-54-217-245-9.eu-west-1.compute.amazonaws.com:5432/dfjf1fo20ep249' })
+      db = PG.connect({ dbname: 'nine_dice', host: DATABASE_URL })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
